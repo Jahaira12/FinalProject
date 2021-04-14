31b314de565b3073e98302193d65ba1d89a7e60a
Jahaira12 <82242646+Jahaira12@users.noreply.github.com>
2021-04-14 16:23:28 -0700

    Update finalproject.java

--- finalproject.java
+++ finalproject.java
@@ -4,8 +4,28 @@ import javax.swing.JFrame;
 
 public class finalproject {
     public static void main(String[] args){
-        JFrame window;
+       		JFrame window;
 		Container con; 
-
-    }
+		JPanel titleNamePanel, 
+	    	JLabel titleNameLabel,
+	    
+	    	Font titleFont = new Font("Times New Roman", Font.PLAIN, 90);
+}
+	public Login_Screen() {
+			//Initialized JFrame to window
+			//sets the size, color, adds a funtion to close the funtion  
+			window = new JFrame();
+			window.setSize(800, 600);
+			window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			window.getContentPane().setBackground(Color.black);
+			window.setLayout(null);
+			window.setVisible(true);
+			con = window.getContentPane();
+		
+			titleNamePanel = new JPanel();
+			titleNamePanel.setBounds(100, 100, 600, 150);
+			titleNamePanel.setBackground(Color.black);
+			titleNameLabel = new JLabel("Login");
+			titleNameLabel.setForeground(Color.blue);
+			titleNameLabel.setFont(titleFont);
 }
